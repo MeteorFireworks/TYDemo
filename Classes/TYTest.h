f1332a08bfd4cd4f74457e3681383d304d1cbaf1
TY <tangy@bairuitech.com>
2020-12-30 12:53:13 +0800

    【标题】私有库提交

---
+++ Classes/TYTest.h
@@ -0,0 +1,16 @@
+//
+//  TYTest.h
+//  TYDemo
+//
+//  Created by xypp01 on 2020/12/30.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TYTest : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Classes/TYTest.m
@@ -0,0 +1,12 @@
+//
+//  TYTest.m
+//  TYDemo
+//
+//  Created by xypp01 on 2020/12/30.
+//
+
+#import "TYTest.h"
+
+@implementation TYTest
+
+@end
